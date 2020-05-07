480f9bb16196104d750432568427973170a0735a
Mrbh <Mrbhmr@gmail.com>
2020-05-07 11:36:15 +0200

    Parse the JSON Response to Tweet Objects

--- TwitterSentiment/TwitterSentiment/Worker.cs
+++ TwitterSentiment/TwitterSentiment/Worker.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace TwitterSentiment
 {
@@ -21,6 +24,7 @@ namespace TwitterSentiment
 
         public override Task StartAsync(CancellationToken cancellationToken)
         {
+            
             client = new HttpClient();
             return base.StartAsync(cancellationToken);
         }
@@ -39,9 +43,17 @@ namespace TwitterSentiment
                 HttpResponseMessage response = client.GetAsync("https://prod-104.westeurope.logic.azure.com/workflows/0fe02adb350546fd8b90d52d0eb5ad60/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=osX5anjLkff-II9SFa8Q9O-S7tucOfbfferPCZOcZDw").Result;
                 Console.WriteLine("Request Message Information:- \n\n" + response.RequestMessage + "\n");
                 Console.WriteLine("Response Message Header \n\n" + response.Content.Headers + "\n");
+                
                 var customerJsonString = await response.Content.ReadAsStringAsync();
                 if (response.IsSuccessStatusCode)
                 {
+                    var arrayOfTweets = JsonConvert.DeserializeObject<List<string>>(customerJsonString);
+                    List<Tweet> tweets = new List<Tweet>();
+                    foreach (var tweet in arrayOfTweets)
+                    {
+                        var elements = tweet.Split("|||");
+                        tweets.Add(new Tweet(elements[0], elements[1], elements[2]));
+                    }
                     _logger.LogInformation($"The website is up. Status code {response.StatusCode}");
                     Console.WriteLine(customerJsonString);
                 }
@@ -49,7 +61,21 @@ namespace TwitterSentiment
                 {
                     _logger.LogError($"The website is down. Status {response.StatusCode}");
                 }
-                await Task.Delay(5000, stoppingToken);
+               
+            }
+        }
+
+        public class Tweet
+        {
+            public Tweet(string id, string txt, string score) => (this.Id, this.Text, this.SentimentScore) = (id,txt,score);
+
+            public string Id;
+            public string Text;
+            public string SentimentScore;
+
+            public override string ToString()
+            {
+                return $"Score: {SentimentScore}";
             }
         }
     }
